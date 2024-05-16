0adc4d9476942d06f0a1f82a249a96850f5f11b8
touahkenza <ktouah19@gmail.com>
2024-05-16 15:57:35 -0600

    resolution du qr-code

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,22 +1,32 @@
-import { Controller, Get, Post, Delete, Put, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Put,
+  Body,
+  Param,
+} from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { CreateTransactionDto } from './transaction.dto';
+import { CreateTransactionMoneyDto } from './transactionmoney.dto';
 
 @Controller('transaction')
 export class TransactionController {
   constructor(private readonly transactionService: TransactionService) {}
 
-  
-
   @Get('getTransaction')
-  getTransaction(): string {
+  async getTransaction() {
     return this.transactionService.getTransaction();
+  }
+  @Get('transferMoney/:idUser/:amount/:idMarchand')
+  async getTransferMoney(@Param() data: CreateTransactionMoneyDto) {
+    console.log("🚀 ~ TransactionController ~ getTransferMoney ~ data:", data)
+    return this.transactionService.transferMoney(data);
   }
 
   @Post('create')
-  async createTransaction(
-    @Body() data: CreateTransactionDto,
-  ) {
+  async createTransaction(@Body() data: CreateTransactionDto) {
     return this.transactionService.saveTransaction(data);
   }
   @Delete('deleteTransaction')
