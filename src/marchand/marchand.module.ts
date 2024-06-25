5dd57683c43f9f5f1182a1e42963ab916d3728de
laetitiazidane <laetitiazidane18@gmail.com>
2024-06-25 14:31:49 +0100

    fix deps

--- src/marchand/marchand.module.ts
+++ src/marchand/marchand.module.ts
@@ -3,10 +3,14 @@ import { MarchandController } from './marchand.controller';
 import { MarchandService } from './marchand.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Marchand } from 'src/entities/marchand.entity';
+import { Account } from 'src/entities/account.entity';
+import { CardService } from 'src/card/card.service';
+import { CardModule } from 'src/card/card.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Marchand])],
+  imports: [TypeOrmModule.forFeature([Marchand, Account]), CardModule],
   controllers: [MarchandController],
   providers: [MarchandService],
+
 })
-export class MarchandModule {}
+export class MarchandModule { }
